d60c5847820c7e34b9c9b1a2945d629e77c3b54f
Stefan Mihajlovski <smihajlovski89@gmail.com>
2016-05-03 17:05:16 +0200

    Trying to implement Google Maps

--- app/src/main/java/com/example/digital05/digitalpoint/ContactUsFragment.java
+++ app/src/main/java/com/example/digital05/digitalpoint/ContactUsFragment.java
@@ -7,14 +7,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
 /**
  * Created by digital05 on 5/3/16.
  */
-public class ContactUsFragment extends Fragment {
+public class ContactUsFragment extends Fragment implements OnMapReadyCallback {
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contact_us, container, false);
+        View v = inflater.inflate(R.layout.fragment_contact_us, container, false);
+
+        return v;
     }
+
+
 }
